a8354330250360d178f2722522e6aedd035a551a
Dean Howe <hideanhowe@gmail.com>
2025-12-17 20:29:00 +0000

    testing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Candlelight
+/// Candlelight
 // Given an integer representing the number of candles you start with, and an integer representing how many burned candles it takes to create a new one, return the number of candles you will have used after creating and burning as many as you can.
 
 // For example, if given 7 candles and it takes 2 burned candles to make a new one:
@@ -11,3 +11,35 @@
 // Recycle 2 leftovers into 1 new candle,
 // Burn 1 candle.
 // You will have burned 13 total candles in the example.
+
+function burnCandles(candles, leftoversNeeded) {
+  console.log(candles, leftoversNeeded);
+  const mod = candles % leftoversNeeded;
+  console.log(candles - mod);
+
+  let burned = candles;
+  let leftovers = 0;
+  let newCandles = 0;
+  let recycled = 0;
+
+  for (let i = 0; i <= candles; i++) {
+    if (burned > 1) {
+      recycled = recycleCandles(burned);
+    }
+  }
+  // return candles;
+  console.log(recycleCandles(7));
+  console.log(leftoversRemaining(7));
+
+  function recycleCandles(num) {
+    return Math.floor(num / leftoversNeeded);
+  }
+
+  function leftoversRemaining(num) {
+    return num % leftoversNeeded;
+  }
+
+  function countCandles(num) {
+    return;
+  }
+}
